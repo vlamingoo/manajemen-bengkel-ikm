385622bedcf421edc41d1a3301ca86669bf7dd1f
vlamingoo <revalinariveladinda@gmail.com>
2025-12-29 10:41:42 +0700

    Update sistem lagi

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,7 +64,7 @@ return [
     */
 
     'logo' => '<b>ZIVIZ</b>',
-    'logo_img' => 'public/img/infinity_ZAFAVELIN.png',
+    'logo_img' => 'img/infinity_ZAFAVELIN.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
